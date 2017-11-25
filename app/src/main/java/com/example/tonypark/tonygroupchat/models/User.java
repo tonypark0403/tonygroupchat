239c6a794a8bc443cc1898359abbab9555b628f4
Tony Park <c3dream@gmail.com>
2017-11-25 11:38:17 -0500

    added google auth using fireabase auth

---
+++ app/src/main/java/com/example/tonypark/tonygroupchat/models/User.java
@@ -0,0 +1,11 @@
+package com.example.tonypark.tonygroupchat.models;
+
+import lombok.Data;
+
+/**
+ * Created by Tony Park on 11/25/2017.
+ */
+@Data
+public class User {
+    String uid, email, name, profileUrl;
+}
