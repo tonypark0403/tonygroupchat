35df5ac4d9e78845cad0b9a85c8156746487a312
Tony Park <c3dream@gmail.com>
2017-11-24 20:51:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tonypark/tonygroupchat/InitActivity.java
@@ -0,0 +1,13 @@
+package com.example.tonypark.tonygroupchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init);
+    }
+}
